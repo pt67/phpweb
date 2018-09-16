185936f994c15d1cdd0b134e96076f18208d49fc
Prakash Thapa <pt6769759@gmail.com>
2018-09-16 16:49:54 +0545

    Add files via upload

---
+++ admin/admin.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Admin</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<header>
+<h1>Admin Panel</h1>
+</header>
+<div class="wrapper">
+<aside class="sidebar">
+<a href="" >Today Status</a>
+<a href="" >visitor status </a>
+<a href="" >User message</a>
+<a href="" >Members count</a>
+<a href="<?php echo "../admin"?>" >Logout</a>
+</aside>
+<section class="main">
+<h3>Present status</h3>
+</section>
+</div>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
